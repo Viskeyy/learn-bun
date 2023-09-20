b949df6df472a86e11a34212d193904f0abacdb3
Viskeyy <50070461+Viskeyy@users.noreply.github.com>
2023-09-20 11:18:02 +0800

    refactor: refactor test func and index global var
    使用 globalThis 替换 globalThis.window, globalThis 在浏览器中就是 window, 不需要再 .window. 更改 WebTransport 的导出方式

--- __test__/webtransport.spec.ts
+++ __test__/webtransport.spec.ts
@@ -1,83 +1,66 @@
 // a unit test for WebTransportPolyfill
 import { describe, expect, test, beforeEach } from 'bun:test';
-import { WebTransportPolyfill } from '../src/index';
+import * as WebTransport from '../src/index';
+
+console.log(WebTransport);
 
 // suppress console.log and console.info
-beforeEach(() => {
-  Object.assign(globalThis, {
-    window: {
-      addEventListener: () => { },
-      console: {
-        log: () => { },
-        info: () => { },
-        debug: () => { },
-      },
-      WebSocket: class WebSocket {
-        constructor() { }
-        addEventListener() { }
-        close() { }
-      },
-    },
-  })
-});
+beforeEach(() => {});
 
 describe('test .ctor', () => {
-  test('should throw SyntaxError when url is not valid', () => {
-    // const err = new SyntaxError("Invalid URL");
-    expect(() => {
-      new WebTransportPolyfill("oooo")
-    }).toThrow(SyntaxError)
-  });
+    test('should throw SyntaxError when url is not valid', () => {
+        // const err = new SyntaxError("Invalid URL");
+        expect(() => new globalThis.WebTransport('oooo')).toThrow(SyntaxError);
+    });
 
-  test('should throw SyntaxError when url is not https', () => {
-    // const err = new SyntaxError("Invalid protocol");
-    expect(() => {
-      new WebTransportPolyfill("wss://lo.yomo.dev:8443")
-    }).toThrow(SyntaxError)
-  });
+    test('should throw SyntaxError when url is not https', () => {
+        // const err = new SyntaxError("Invalid protocol");
+        expect(
+            () => new globalThis.WebTransport('wss://lo.yomo.dev:8443')
+        ).toThrow(SyntaxError);
+    });
 
-  test('should throw SyntaxError when url has fragement', () => {
-    // const err = new SyntaxError("Fragment is not permitted");
-    expect(() => {
-      new WebTransportPolyfill("https://lo.yomo.dev:8443/#abced")
-    }).toThrow(SyntaxError)
-  });
+    test('should throw SyntaxError when url has fragement', () => {
+        // const err = new SyntaxError("Fragment is not permitted");
+        expect(
+            () => new globalThis.WebTransport('https://lo.yomo.dev:8443/#abced')
+        ).toThrow(SyntaxError);
+    });
 
-  test('should work', () => {
-    new WebTransportPolyfill("https://lo.yomo.dev:8443/v1");
-  });
-})
+    test('should work', () =>
+        new globalThis.WebTransport('https://lo.yomo.dev:8443/v1'));
+});
 
 describe('test close()', () => {
-  test('should close the connection', () => {
-    const wt = new WebTransportPolyfill("https://lo.yomo.dev:8443");
-    wt.close();
-  })
+    test('should close the connection', () => {
+        const wt = new globalThis.WebTransport('https://lo.yomo.dev:8443');
+        wt.close();
+    });
 
-  test('should close the connection with code and reason', () => {
-    const wt = new WebTransportPolyfill("https://lo.yomo.dev:8443");
-    wt.close({ closeCode: 4321, reason: "test" });
-  });
-})
+    test('should close the connection with code and reason', () => {
+        const wt = new globalThis.WebTransport('https://lo.yomo.dev:8443');
+        wt.close({ closeCode: 4321, reason: 'test' });
+    });
+});
 
 describe('test server initiated stream', () => {
-  test('incomingBidirectionalStreams', async () => {
-    const wt = new WebTransportPolyfill("https://lo.yomo.dev:8443");
-    const rs = wt.incomingBidirectionalStreams;
-    const err = new Error("websocket do not support server initiated stream")
-    // assert.throws(() => { rs.getReader() }, err)
-    expect(() => {
-      rs.getReader()
-    }).toThrow()
-  })
+    test('incomingBidirectionalStreams', async () => {
+        const wt = new globalThis.WebTransport('https://lo.yomo.dev:8443');
+        const rs = wt.incomingBidirectionalStreams;
+        const err = new Error(
+            'websocket do not support server initiated stream'
+        );
+        // assert.throws(() => { rs.getReader() }, err)
+        expect(() => rs.getReader()).toThrow();
+    });
 
-  test('incomingUniidirectionalStreams', async () => {
-    const wt = new WebTransportPolyfill("https://lo.yomo.dev:8443");
-    const rs = wt.incomingUnidirectionalStreams;
-    const err = new Error("websocket do not support server initiated stream")
-    // assert.throws(() => { rs.getReader() }, err)
-    expect(() => {
-      rs.getReader()
-    }).toThrow()
-  })
-})
+    test('incomingUniidirectionalStreams', async () => {
+        const wt = new globalThis.WebTransport('https://lo.yomo.dev:8443');
+        const rs = wt.incomingUnidirectionalStreams;
+        const err = new Error(
+            'websocket do not support server initiated stream'
+        );
+        // assert.throws(() => { rs.getReader() }, err)
+        expect(() => rs.getReader()).toThrow();
+    });
+});
